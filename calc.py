66f9fa66b8c14e06c787bc982cd83054c4066873
jiagengchang <jiageng1997@gmail.com>
2022-04-09 17:03:55 +0800

    initial commit

---
+++ calc.py
@@ -0,0 +1,35 @@
+r = input('Percentage annual rate of return: ')
+r= int(r)
+P = input('Principle amount: ')
+P = int(P)
+inv = input('Amount invested regularly: ')
+inv = int(inv)
+freq = input('Number of investments per year: ')
+freq = int(freq)
+t = input('Number of years: ')
+t = int(t)
+
+
+
+year_amount = {}
+year_amount['Year 0'] = P
+start = P
+for i in range(1,t+1):
+    end = (start + freq*inv)*(1+r)
+    year_amount['Year ',str(i)] = end
+    start = end
+
+print(year_amount)
+
+
+
+
+
+
+
+
+
+
+
+
+
